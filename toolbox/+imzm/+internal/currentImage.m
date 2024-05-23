4c99893d54ddc26fee13ec8dbf82e74e82fd2cb3
eddins <4043689+eddins@users.noreply.github.com>
2024-05-23 19:24:50 -0400

    Add doc for currentImage. Return an empty image object instead of an empty placeholder object.

--- toolbox/+imzm/+internal/currentImage.m
+++ toolbox/+imzm/+internal/currentImage.m
@@ -1,5 +1,14 @@
+%currentImage First image found in current axes of current figure
+%
+%   im = currentImage returns the first graphics image object found in the
+%   current axes of the current figure. The output is empty if:
+%
+%   - There is no current figure, or
+%   - The current figure does not have a current axes, or
+%   - The current axes of the current figure does not contain an image
+
 function im = currentImage
-    im = gobjects(1,0);
+    im = matlab.graphics.primitive.Image.empty(1,0);
     r = groot;
     fig = r.CurrentFigure;
     if ~isempty(fig)
