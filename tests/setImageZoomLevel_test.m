ca4ede3a6f0b6a9ecf9d67ac5e7e61c73a781241
eddins <4043689+eddins@users.noreply.github.com>
2024-05-27 15:51:09 -0400

    Implement code to error if zoom level cannot be set successfully. Add doc. Update implementation with function name changes. Add test.

---
+++ tests/setImageZoomLevel_test.m
@@ -0,0 +1,60 @@
+classdef setImageZoomLevel_test < matlab.unittest.TestCase
+
+    properties
+        TestFigure
+    end
+
+    methods (TestMethodSetup)
+        function createFigure(test_case)
+            test_case.TestFigure = figure(Units = "pixels", ...
+                Position = [100 100 800 200]);
+            addTeardown(test_case,@() close(test_case.TestFigure));
+        end
+    end
+
+    methods (Test)
+        function basicTest(test_case)
+            ax = axes(Units = "pixels", Position = [100 100 50 50]);
+            im = imagesc(ones(10,25),Parent = ax);
+            setImageZoomLevel([100 100],im);
+            drawnow
+
+            actual = getImageZoomLevel(im);
+            expected = [100 100];
+            test_case.verifyEqual(actual,expected);
+        end
+
+        function scalarInput(test_case)
+            ax = axes(Units = "pixels", Position = [100 100 50 50]);
+            im = imagesc(ones(10,25),Parent = ax);
+            setImageZoomLevel(100,im);
+            drawnow
+
+            actual = getImageZoomLevel(im);
+            expected = [100 100];
+            test_case.verifyEqual(actual,expected);            
+        end
+
+        function dataAspectRatioModeManual(test_case)
+            ax = axes(Units = "pixels", Position = [100 100 50 50]);
+            im = imagesc(ones(10,25),Parent = ax);
+            ax.DataAspectRatioMode = "manual";
+            drawnow
+
+            setImageZoomLevel(100,im);
+            actual = getImageZoomLevel(im);
+            expected = [100 100];
+            test_case.verifyEqual(actual,expected);             
+        end
+
+        function dataAspectRatioModeManualErrorCase(test_case)
+            ax = axes(Units = "pixels", Position = [100 100 50 50]);
+            im = imagesc(ones(10,25),Parent = ax);
+            ax.DataAspectRatioMode = "manual";
+            drawnow
+
+            test_case.verifyError(@() setImageZoomLevel([100 125],im),...
+                "imzm:InvalidRatioChange");            
+        end        
+    end
+end
--- toolbox/setImageZoomLevel.m
+++ toolbox/setImageZoomLevel.m
@@ -1,15 +1,52 @@
-function setImageZoomLevel(scale,im)
+%setImageZoomLevel Set image zoom level
+%
+%   setImageZoomLevel(level) sets the zoom level of the image in the
+%   current axes in the current figure. Most commonly, the zoom level is
+%   set as a scalar, specified as a percentage, that applies in both the
+%   horizontal and vertical directions. A zoom level of 100 means the image
+%   pixel is the same size as a screen pixel. The level can also be a
+%   two-element vector, [level_x level_y], that specifies the zoom level
+%   independently for the horizontal and vertical directions.
+%
+%   setImageZoomLevel(level,im) sets the zoom level of the specified
+%   graphics image object.
+%
+%   NOTE: if the axes DataAspectRatioMode is 'manual', then setting the
+%   zoom level so that the ratio between the horizontal and vertical zoom
+%   levels changes is not possible, and an error message will result.
+%
+%   See also getImageZoomLevel
+
+% Steve Eddins
+
+function setImageZoomLevel(new_level,im)
     arguments
-        scale (1,2) double {mustBeReal, mustBeFinite}
-        im    (1,1) matlab.graphics.primitive.Image = currentImage
+        new_level (1,2) double {mustBeReal, mustBeFinite}
+        im    (1,1) matlab.graphics.primitive.Image = imzm.internal.currentImage
     end
 
-    current_scale = getImagePixelDisplayScale(im);
-    s = current_scale ./ scale;
-
+    current_level = getImageZoomLevel(im);
     ax = ancestor(im,"axes");
-    c = getAxesDisplayCenter(ax);
+
+    if ax.DataAspectRatioMode == "manual"
+        current_level_ratio = current_level(1) / current_level(2);
+        new_level_ratio = new_level(1) / new_level(2);
+        if ~almostEqual(current_level_ratio,new_level_ratio)
+            error("imzm:InvalidRatioChange",...
+                "Cannot change the x/y ratio of the zoom level when " + ...
+                "the axes DataAspectRatioMode is ""manual"".")
+        end
+    end
+    
+    s = current_level ./ new_level;
+
+    c = getAxesCenterXY(ax);
 
     ax.XLim = s(1) * (ax.XLim - c(1)) + c(1);
     ax.YLim = s(2) * (ax.YLim - c(2)) + c(2);
+end
+
+function tf = almostEqual(a,b)
+    tol = eps(class(a))^0.75;
+    tf = abs(a - b) <= tol*max(abs(a),abs(b));
 end
