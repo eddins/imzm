c661cb57d251bb03c8b55dd4ef4265a325f6f49a
eddins <4043689+eddins@users.noreply.github.com>
2025-02-13 08:16:36 -0500

    Perform the calculation on the embedded axes if running in a live script.

--- toolbox/+imzm/+internal/axesPlotAreaExtentPixels.m
+++ toolbox/+imzm/+internal/axesPlotAreaExtentPixels.m
@@ -13,15 +13,19 @@ function extent = axesPlotAreaExtentPixels(ax)
         ax (1,1) matlab.graphics.axis.Axes
     end
 
-    original_units = ax.Units;
-    unit_restorer = onCleanup(@() setAxesUnits(ax,original_units));
-    ax.Units = "pixels";
-    pixel_position = tightPosition(ax);
+    true_ax = imzm.internal.getTrueAxes(ax);
+
+    original_units = true_ax.Units;
+    unit_restorer = onCleanup(@() setAxesUnits(true_ax,original_units));
+    true_ax.Units = "pixels";
+    pixel_position = tightPosition(true_ax);
     extent = pixel_position(3:4);
 end
 
 function setAxesUnits(ax,units)
-    ax.Units = units;
+    if isgraphics(ax)
+        ax.Units = units;
+    end
 end
 
 % Copyright (c) 2024 Steven L. Eddins
